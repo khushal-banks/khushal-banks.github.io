fcb598be36e6cb9a9800cd3b486d549632e7197d
Khushal <khushal.banks@gmail.com>
2024-05-12 14:01:09 +0530

    Updated docs for logbank_c

--- docs/logbank_c/doxygen/html/search/enumvalues_0.js
+++ docs/logbank_c/doxygen/html/search/enumvalues_0.js
@@ -1,9 +1,9 @@
 var searchData=
 [
-  ['log_5fdebug_0',['LOG_DEBUG',['../logbank_8h.html#a6cfcf0d1e54151523d73f747ea51ba99ab9f002c6ffbfd511da8090213227454e',1,'logbank.h']]],
-  ['log_5ferror_1',['LOG_ERROR',['../logbank_8h.html#a6cfcf0d1e54151523d73f747ea51ba99a230506cce5c68c3bac5a821c42ed3473',1,'logbank.h']]],
-  ['log_5ffatal_2',['LOG_FATAL',['../logbank_8h.html#a6cfcf0d1e54151523d73f747ea51ba99ac630750884d91cb9767ef2200bbb048b',1,'logbank.h']]],
-  ['log_5finfo_3',['LOG_INFO',['../logbank_8h.html#a6cfcf0d1e54151523d73f747ea51ba99a6e98ff471e3ce6c4ef2d75c37ee51837',1,'logbank.h']]],
-  ['log_5fissue_4',['LOG_ISSUE',['../logbank_8h.html#a6cfcf0d1e54151523d73f747ea51ba99a97cc9d9270e158d4d00f51e44229ed67',1,'logbank.h']]],
-  ['log_5fwarn_5',['LOG_WARN',['../logbank_8h.html#a6cfcf0d1e54151523d73f747ea51ba99ac8041ffa22bc823d4726701cdb13fc13',1,'logbank.h']]]
+  ['log_5fdebug_0',['LOG_DEBUG',['../logbank_8h.html#a6cfcf0d1e54151523d73f747ea51ba99a5b7d4f0d91e50288752648b12035fb35',1,'logbank.h']]],
+  ['log_5ferror_1',['LOG_ERROR',['../logbank_8h.html#a6cfcf0d1e54151523d73f747ea51ba99a3a32823436abb3c3442b6bf3f70ec9b6',1,'logbank.h']]],
+  ['log_5ffatal_2',['LOG_FATAL',['../logbank_8h.html#a6cfcf0d1e54151523d73f747ea51ba99a586842e9a80356d3447729860d4417bc',1,'logbank.h']]],
+  ['log_5finfo_3',['LOG_INFO',['../logbank_8h.html#a6cfcf0d1e54151523d73f747ea51ba99a1d2ddc2d110200d6c342359bd0e19a3c',1,'logbank.h']]],
+  ['log_5fissue_4',['LOG_ISSUE',['../logbank_8h.html#a6cfcf0d1e54151523d73f747ea51ba99a31029d55440c470cf8cdbb42787e34fd',1,'logbank.h']]],
+  ['log_5fwarn_5',['LOG_WARN',['../logbank_8h.html#a6cfcf0d1e54151523d73f747ea51ba99aaf5ddc798d2875139456400b5c18897d',1,'logbank.h']]]
 ];
--- docs/logbank_c/doxygen/html/search/functions_0.js
+++ docs/logbank_c/doxygen/html/search/functions_0.js
@@ -1,8 +1,8 @@
 var searchData=
 [
-  ['log_5fget_5fdefault_5flevel_0',['log_get_default_level',['../logbank_8h.html#a5e49b5594c4aed69441bc2b4b0e8e0e7',1,'log_get_default_level(void):&#160;logbank.c'],['../logbank_8c.html#a5e49b5594c4aed69441bc2b4b0e8e0e7',1,'log_get_default_level(void):&#160;logbank.c']]],
-  ['log_5fget_5flevel_1',['log_get_level',['../logbank_8h.html#ab53553042bb3122475a03674bd689d5a',1,'log_get_level(void):&#160;logbank.c'],['../logbank_8c.html#ab53553042bb3122475a03674bd689d5a',1,'log_get_level(void):&#160;logbank.c']]],
-  ['log_5fget_5flevel_5fname_2',['log_get_level_name',['../logbank_8h.html#a1479d11a4b2d1a14dd439feb23c79ece',1,'log_get_level_name(int level):&#160;logbank.c'],['../logbank_8c.html#a1479d11a4b2d1a14dd439feb23c79ece',1,'log_get_level_name(int level):&#160;logbank.c']]],
-  ['log_5fset_5flevel_3',['log_set_level',['../logbank_8h.html#af0d39913e824d4d0aef7513b284ea289',1,'log_set_level(int level):&#160;logbank.c'],['../logbank_8c.html#af0d39913e824d4d0aef7513b284ea289',1,'log_set_level(int level):&#160;logbank.c']]],
-  ['log_5fthis_4',['log_this',['../logbank_8h.html#a900f2b33063f0d4f9090ef6d47c0ead0',1,'log_this(int level, const char *fmt,...):&#160;logbank.c'],['../logbank_8c.html#a900f2b33063f0d4f9090ef6d47c0ead0',1,'log_this(int level, const char *fmt,...):&#160;logbank.c']]]
+  ['log_5fget_5fdefault_5flevel_0',['log_get_default_level',['../logbank_8h.html#a0235f5d0111c171178d8cfaeedc8c0af',1,'log_get_default_level(void):&#160;logbank.c'],['../logbank_8c.html#a5e49b5594c4aed69441bc2b4b0e8e0e7',1,'log_get_default_level(void):&#160;logbank.c']]],
+  ['log_5fget_5flevel_1',['log_get_level',['../logbank_8h.html#a96925309953d5ca93fc4e5b20a557ffb',1,'log_get_level(void):&#160;logbank.c'],['../logbank_8c.html#ab53553042bb3122475a03674bd689d5a',1,'log_get_level(void):&#160;logbank.c']]],
+  ['log_5fget_5flevelname_2',['log_get_levelname',['../logbank_8h.html#a2d07ede7701c6fd6731213b5f85f6d29',1,'log_get_levelname(int level):&#160;logbank.c'],['../logbank_8c.html#a2f37fc38d4885ae4fd5f499787e5f550',1,'log_get_levelname(int level):&#160;logbank.c']]],
+  ['log_5fset_5flevel_3',['log_set_level',['../logbank_8h.html#a610cc9f0715471a7599e7f5f7e40a87f',1,'log_set_level(int level):&#160;logbank.c'],['../logbank_8c.html#af0d39913e824d4d0aef7513b284ea289',1,'log_set_level(int level):&#160;logbank.c']]],
+  ['log_5fthis_4',['log_this',['../logbank_8h.html#a900f2b33063f0d4f9090ef6d47c0ead0',1,'log_this(int level, const char *fmt,...):&#160;logbank.h'],['../logbank_8c.html#a900f2b33063f0d4f9090ef6d47c0ead0',1,'log_this(int level, const char *fmt,...):&#160;logbank.c']]]
 ];
