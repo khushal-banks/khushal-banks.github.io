56b4667c5fb9e0016db61337fd9fe09a93bfa334
Khushal <khushal.banks@gmail.com>
2024-05-18 12:53:32 +0530

    Removed old docs for logbank_c

--- docs/logbank_c/doxygen/html/search/files_5.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['snippets_5fheader_2eh_0',['snippets_header.h',['../snippets__header_8h.html',1,'']]]
-];
--- docs/logbank_c/doxygen/html/search/pages_7.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['updates_0',['Major logbank_c updates',['../md_CHANGE__LIST.html',1,'']]]
-];
